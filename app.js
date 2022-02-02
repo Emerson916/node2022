edb33f61afd94cdaf44a284c278265d532241479
Emerson916 <emersons.a296@gmail.com>
2022-02-02 16:40:32 -0300

    Criando o primeiro objeto em node com express

--- app.js
+++ app.js
@@ -2,10 +2,34 @@ const express = require("express")
 
 const app = express();
 
-app.get("/rota", (req, res) => {
-    return res.json({
-        message: "Acessou a rota, show de boola !!"
+// app.get("/rota", (req, res) => {
+//     return res.json({
+//         message: "Acessou a rota, show de boola !!"
+//     })
+// })
+
+const products = [
+
+/**
+ * POST => Inserir um dado
+ * GET => Buscar um/mais dados
+ * PUT => Alterar um dado
+ * DELETE => Remover um dado
+ */
+
+
+/** 
+ * BODY => Sempre que eu quiser enviar dados para minha aplicação
+ * Params => Parametros de rotas, fica na URL {EX: (/product/1)}
+ * Query => Parametros não obrigatorios, passado na URL {EX: (/pruduct?id=13131&value=2121)}
+*/
+
+    app.post("/products", (req, res) => {
+        //nome, preço e descrição
+
+
     })
-})
+
+];
 
 app.listen(4002, () => console.log("Funciona"));
